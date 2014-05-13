23b28400f7b5cc7d5284e270b579dafcf563adc9
Dandlezzz <danieladammiller@gmail.com>
2014-05-13 11:49:13 -0700

    modified games controller

--- app/controllers/api/games_controller.rb
+++ app/controllers/api/games_controller.rb
@@ -14,7 +14,7 @@ private
 	def restrict_access
 		unless  ApiKey.exists?(access_token: params[:api_key])
 		  authenticate_or_request_with_http_token do |token, options|
-		  ApiKey.exists?(access_token: token)
+		  	ApiKey.exists?(access_token: token)
 		  end
 		end
 	end
