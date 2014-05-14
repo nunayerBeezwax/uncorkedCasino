89c4bc21fc62ca2cc1107f2b7ea9a4abec8618b6
Dandlezzz <danieladammiller@gmail.com>
2014-05-14 14:18:57 -0700

    initial branch commit

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -4,30 +4,22 @@ class Api::SessionsController < Devise::RegistrationsController
   respond_to :json
   skip_before_filter :verify_authenticity_token
 
-  devise :timeoutable, :timeout_in => 1440.minutes
-
   def create
     build_resource
-<<<<<<< HEAD
+    
     resource =  User.find_for_database_authentication(
       :username => params[:user][:username]
-=======
-   
-    resource =  User.find_for_database_authentication(
-      username: params[:user][:username]
->>>>>>> 580e8691626bed247a39df63aab8fc1101c6e52e
-    )
+      )
     return invalid_login_attempt unless resource
 
     if resource.valid_password?(params[:user][:password])
       sign_in("user", resource)
       render json: {
         success: true,
-<<<<<<< HEAD
+
         auth_token: ApiKey.create(:user_id =>resource.id),
-=======
+
         auth_token: ApiKey.create(user_id: resource.id),
->>>>>>> 580e8691626bed247a39df63aab8fc1101c6e52e
         email: resource.email
         # username too?
       }
