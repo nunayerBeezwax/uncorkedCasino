4b223d7b47b725f7b7d1f27b31aae6fede84c613
Dandlezzz <danieladammiller@gmail.com>
2014-05-15 10:10:07 -0700

    deleted comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,23 +33,4 @@ class User < ActiveRecord::Base
   def signed_in?
     return true if ApiKey.find_by(user_id: self.id) else false
   end
-
-
- 
- #  def login=(login)
- #    @login = login
- #  end
-
- #  def login
- #    @login || self.username || self.email
- #  end
-
- #  def self.find_first_by_auth_conditions(warden_conditions)
-  #   conditions = warden_conditions.dup
-  #   if login = conditions.delete(:login)
-  #     where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
-  #   else
-  #     where(conditions).first
-  #   end
-  # end
 end
